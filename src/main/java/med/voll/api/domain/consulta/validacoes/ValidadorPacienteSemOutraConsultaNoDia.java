d47c619bd71edc07f79ac5baa33b4a55551c92af
Alexandre Esteves <alexandreestevesfclaro@gmail.com>
2023-10-29 18:32:22 -0300

    Validação de paciente sem outra consulta no dia

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorPacienteSemOutraConsultaNoDia.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoExcpetion;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+public class ValidadorPacienteSemOutraConsultaNoDia {
+
+    private ConsultaRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var primeiroHorario = dados.data().withHour(7);
+        var ultimoHorario = dados.data().withHour(18);
+        var pacientePossuiOutraConsultaNoDia = repository.existsByPacienteIdAndDataBetween(dados.idPaciente(), primeiroHorario, ultimoHorario);
+        if (pacientePossuiOutraConsultaNoDia) {
+            throw new ValidacaoExcpetion("O paciente já possui uma consulta agendada nesse dia!");
+        }
+
+    }
+
+}
